2a2c7dffb1d62663a66fa7b708559d8136a6b3e3
sokinpui <sosokinpui@gmail.com>
2025-09-08 00:05:39 +0800

    chdir to git repository root coder now must use inside a git repository

--- cmd/coder/main.go
+++ cmd/coder/main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"coder/internal/logger"
 	"coder/internal/ui"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
+func isGitRepository() bool {
+	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	return strings.TrimSpace(string(output)) == "true"
+}
+
 func main() {
+	if !isGitRepository() {
+		fmt.Fprintln(os.Stderr, "Error: This application must be run from within a git repository.")
+		os.Exit(1)
+	}
 	logger.Init()
 	ui.Start()
 }
