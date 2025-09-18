3b119a05dfbfbadff3f96ec64f6245938d1d15c0
sokinpui <sosokinpui@gmail.com>
2025-09-18 14:43:25 +0800

    web-ui use random port

--- cmd/coder-web/main.go
+++ cmd/coder-web/main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"net"
 	"net/http"
 	"os"
 )
@@ -18,7 +19,7 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Error: This application must be run from within a git repository.")
 		os.Exit(1)
 	}
-	addr := flag.String("addr", ":8084", "http service address")
+	addr := flag.String("addr", ":0", "http service address. Defaults to a random unused port.")
 	flag.Parse()
 
 	logger.Init()
@@ -32,8 +33,24 @@ func main() {
 	}
 	http.Handle("/", http.FileServer(http.FS(distFS)))
 
-	log.Printf("Starting web server on %s", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	listener, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalf("failed to create listener: %v", err)
+	}
+
+	port := listener.Addr().(*net.TCPAddr).Port
+	host := "localhost"
+
+	reqHost, _, err := net.SplitHostPort(*addr)
+	if err == nil && reqHost != "" && reqHost != "0.0.0.0" && reqHost != "::" {
+		host = reqHost
+	}
+
+	serverURL := fmt.Sprintf("http://%s:%d", host, port)
+	log.Printf("Starting web server on %s", serverURL)
+	fmt.Printf("Coder-web is running on: %s\n", serverURL)
+
+	if err := http.Serve(listener, nil); err != nil {
 		log.Fatalf("ListenAndServe: %v", err)
 	}
 }
