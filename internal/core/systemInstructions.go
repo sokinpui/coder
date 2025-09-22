a7f864de4665d3d91d1e07a34a4ced3411046588
sokinpui <sosokinpui@gmail.com>
2025-09-22 13:32:01 +0800

    rename files to txt

--- internal/core/systemInstructions.go
+++ internal/core/systemInstructions.go
@@ -2,17 +2,20 @@ package core
 
 import _ "embed"
 
-//go:embed Roles/coding.md
+//go:embed Roles/coding.txt
 var CodingRole string
 
-//go:embed Roles/documenting.md
+//go:embed Roles/documenting.txt
 var DocumentingRole string
 
-//go:embed Roles/auto-mode.md
+//go:embed Roles/askAI.txt
+var AskAIRole string
+
+//go:embed Roles/auto-mode.txt
 var AutoRole string
 
-//go:embed Instructions.md
+//go:embed Instructions.txt
 var CoderInstructions string
 
-//go:embed titleGenerate.md
+//go:embed titleGenerate.txt
 var TitleGenerationPrompt string
