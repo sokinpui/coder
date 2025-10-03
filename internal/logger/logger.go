6dfe1840e85338e5e61421e9d6294e9e0d729ee1
sokinpui <sosokinpui@gmail.com>
2025-10-03 09:37:41 +0800

    log in .coder/coder.log

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,20 +1,38 @@
 package logger
 
 import (
+	"coder/internal/utils"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-const logFileName = "coder.log"
+const (
+	coderDirName = ".coder"
+	logFileName  = "coder.log"
+)
 
 // Init sets up the global logger to write to a file.
 func Init() {
-	// If the file doesn't exist, create it, or append to the file
-	file, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	repoRoot, err := utils.FindRepoRoot()
 	if err != nil {
-		log.Fatalf("failed to open log file %s: %v", logFileName, err)
+		// If we can't find the repo root, we can't create the log file in the desired location.
+		// This is a fatal error as logging is essential and other parts of the app will also fail.
+		log.Fatalf("failed to find git repository root to initialize logger: %v", err)
+	}
+
+	coderPath := filepath.Join(repoRoot, coderDirName)
+	if err := os.MkdirAll(coderPath, 0755); err != nil {
+		log.Fatalf("failed to create .coder directory for log file: %v", err)
+	}
+
+	logFilePath := filepath.Join(coderPath, logFileName)
+	// If the file doesn't exist, create it, or append to the file
+	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("failed to open log file %s: %v", logFilePath, err)
 	}
 
 	log.SetOutput(file)
-	log.Println("Logger initialized. Logging to", logFileName)
+	log.Println("Logger initialized. Logging to", logFilePath)
 }
